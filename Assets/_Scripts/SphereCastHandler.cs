0c70730fac6b2be8c51a66a1dedda38eece09e1c
TungVLe <tunle0801@gmail.com>
2024-05-26 17:45:27 +0700

    audio points added to contacts

--- Assets/_Scripts/SphereCastHandler.cs
+++ Assets/_Scripts/SphereCastHandler.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
 public class SphereCastHandler : MonoBehaviour
 {
-    public float growthSpd;
+    private float growthSpd;
+    public GameObject contactPointPrefab;
     private SphereCollider m_sphere;
 
     private void Awake()
     {
         m_sphere = GetComponent<SphereCollider>();
+        m_sphere.isTrigger= true;
+        resetSphere();
     }
+    private void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.One))
+        {
+            StartSphereGrow();
+        }
+    }
+
+    
     private void FixedUpdate()
     {
         m_sphere.radius += Time.deltaTime * growthSpd;
+        if (m_sphere.radius > 8)
+        {
+            resetSphere();
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        var contactPoint = other.ClosestPointOnBounds(this.transform.position);
+        var point = Instantiate(contactPointPrefab, contactPoint, Quaternion.identity);
+        Destroy(point, 3);
+    }
+
+    [ContextMenu("StartSphereGrow")]
+    public void StartSphereGrow()
+    {
+        resetSphere();
+        growthSpd = 0.5f;
+    }
+    private void resetSphere()
+    {
+        //reset
+        growthSpd = 0f;
+        m_sphere.radius = 0f;
     }
 }
