8f980349bfc3440c249188703f7f7ff403090b18
TungVLe <tunle0801@gmail.com>
2025-01-09 13:38:28 -0500

    feat: doorway generation

--- Assets/MainTest/SpawnVirtualRoom.cs
+++ Assets/MainTest/SpawnVirtualRoom.cs
@@ -5,10 +5,15 @@ using System.Threading.Tasks;
 using Meta.XR.MRUtilityKit;
 using Oculus.Platform;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnVirtualRoom : MonoBehaviour
 {
-    //[SerializeField] private GameObject prefab;
+    private void OnValidate() {
+        ValidateRoomGenerationParameters();
+    }
+
+  
     private void Start() {
         var room = SpawnRoom();
         MRUK.Instance.LoadSceneFromPrefab(room, true);
@@ -36,47 +41,108 @@ public class SpawnVirtualRoom : MonoBehaviour
     #endregion
 
     #region Generate Room
+    
+    [Serializable]
+    public struct DoorwayData {
+        public float width;
+        public float positionLeftOffset; // Distance from left edge of wall
+    }
+    [Serializable]
+    public struct RoomSizeData {
+        public float width; // x axis
+        public float length; // z axis
+    }
     [Serializable]
     public struct RoomGeneratorData
     {
-        public float width; // x axis
-        public float length; // z axis
+        public RoomSizeData roomSize;
         public float wallHeight;
         public Transform wallPrefab;
+
+        public GenerationLimit doorWidthGen;
+        internal DoorwayData[] doorwayData;
     }
     [SerializeField] private RoomGeneratorData _data;
+
+    private void ValidateRoomGenerationParameters()
+    {
+        if (_data.doorWidthGen.max > _data.roomSize.width) throw new Exception("Door width cannot be greater than room width");
+    }
+
 
     [ContextMenu("Spawn Room")]
     private GameObject SpawnRoom()
     {
-        var room = new GameObject("Custom Room").transform;
+        var room = new GameObject("Custom Room");
+        GenerateFourWalls(room.transform);
+        GenerateDoorways(room.transform);
+
+        room.SetActive(false);
+        return room;
+    }
+
+
+    private void GenerateFourWalls(Transform room) {
         var top = Instantiate(_data.wallPrefab, room);
-        top.localPosition = new Vector3(_data.width / 2, 0, _data.length / 2);
+        var width = _data.roomSize.width;
+        var length = _data.roomSize.length;
+        top.localPosition = new Vector3(width / 2, 0, length / 2);
         top.localEulerAngles = Vector3.zero;
-        top.localScale = new Vector2(_data.width, _data.wallHeight);
+        top.localScale = new Vector3(width, _data.wallHeight,1);
 
         var bottom = Instantiate(_data.wallPrefab, room);
-        bottom.localPosition = new Vector3(_data.width / 2, 0, -1 * _data.length / 2);
-        bottom.localEulerAngles = new Vector3(0,180,0);
-        bottom.localScale = new Vector2(_data.width, _data.wallHeight);
+        bottom.localPosition = new Vector3(width / 2, 0, -1 * length / 2);
+        bottom.localEulerAngles = new Vector3(0, 180, 0);
+        bottom.localScale = new Vector3(width, _data.wallHeight,1);
 
         var left = Instantiate(_data.wallPrefab, room);
         left.localPosition = Vector3.zero;
         left.localEulerAngles = new Vector3(0, -90, 0);
-        left.localScale = new Vector2(_data.length, _data.wallHeight);
+        left.localScale = new Vector3(length, _data.wallHeight,1);
 
         var right = Instantiate(_data.wallPrefab, room);
-        right.localPosition = new Vector3(_data.width, 0, 0);
+        right.localPosition = new Vector3(width, 0, 0);
         right.localEulerAngles = new Vector3(0, 90, 0);
-        right.localScale = new Vector2(_data.length, _data.wallHeight);
+        right.localScale = new Vector3(length, _data.wallHeight, 1);
 
-        for (int i=0; i<room.childCount;i++) {
+        for (int i = 0; i < room.childCount; i++)
+        {
             room.GetChild(i).name = "WALL_FACE";
         }
+    }
 
-        room.gameObject.SetActive(false);
-        return room.gameObject;
+    private void GenerateDoorways(Transform room)
+    {
+        var Zpos = Random.Range(-_data.roomSize.length / 2, _data.roomSize.length / 2);
+        var doorWidth = _data.doorWidthGen.GetRandomVal();
+        var leftOffset = Random.Range(0f, _data.roomSize.width - doorWidth);
+        var leftWallSize = leftOffset;
+        var leftWallPos = new Vector3(leftWallSize / 2, 0, Zpos);
+
+        var leftWall = Instantiate(_data.wallPrefab, room);
+        leftWall.localPosition = leftWallPos;
+        leftWall.localScale = new(leftWallSize, _data.wallHeight , 1);
+
+        var rightWallSize = _data.roomSize.width - leftWallSize - doorWidth;
+        var rightWallPos = new Vector3(leftWallSize + doorWidth + rightWallSize / 2, 0, Zpos);
+        if (rightWallPos.x < _data.roomSize.width) {
+            var rightWall = Instantiate(_data.wallPrefab, room);
+            rightWall.localPosition = rightWallPos;
+            rightWall.localScale = new(rightWallSize, _data.wallHeight, 1);
+        }
+
     }
     #endregion
 
+}
+
+[Serializable]
+public class GenerationLimit
+{
+    [SerializeField] internal float min;
+    [SerializeField] internal float max;
+
+    public float GetRandomVal() {
+        return Random.Range(min, max);
+    }
 }
