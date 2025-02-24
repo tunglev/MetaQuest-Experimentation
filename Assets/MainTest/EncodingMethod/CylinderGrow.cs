7318b4b51e7cda1c5952645663951309a970b676
TungVLe <tunle0801@gmail.com>
2025-02-24 00:18:50 -0500

    feat: human audio with corner fix

--- Assets/MainTest/EncodingMethod/CylinderGrow.cs
+++ Assets/MainTest/EncodingMethod/CylinderGrow.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Meta.XR.MRUtilityKit;
 using UnityEngine;
 
@@ -41,8 +43,9 @@ public class CylinderGrow : EncodingMethod
     }
 
     const int DEFAULT_LAYER_ONLY_MASK = 1 << 0; // default layer is 0
-
     const bool ONLY_VISIBLE = true;
+    private readonly HashSet<int> usedColliderSet = new();
+
     private void HandleTriggerWithAnAnchor(Collider other)
     {
         if (_curGrowSpd == 0f) return;
@@ -50,9 +53,14 @@ public class CylinderGrow : EncodingMethod
         var contactPoint = other.ClosestPointOnBounds(_cylinder.transform.position);
         Vector3 camPos = Camera.main.transform.position;
         Vector3 eyeToContactPoint = contactPoint - camPos;
+        bool isCorner = false;
         if (ONLY_VISIBLE) {
             var hits = Physics.RaycastAll(camPos, eyeToContactPoint, maxDistance: eyeToContactPoint.magnitude + 0.15f, layerMask: DEFAULT_LAYER_ONLY_MASK, QueryTriggerInteraction.Ignore);
-            if (hits.Length != 1) return; //corner in a 3x3
+            if (hits.Length != 1) isCorner = true;
+            foreach (var hit in hits) {
+                if (usedColliderSet.Contains(hit.colliderInstanceID)) return;
+                usedColliderSet.Add(hit.colliderInstanceID);
+            }
         }
         // RaycastHit hit;
         // if (ONLY_VISIBLE && Physics.Raycast(camPos, eyeToContactPoint, hitInfo: out hit, eyeToContactPoint.magnitude, DEFAULT_LAYER_ONLY_MASK, QueryTriggerInteraction.Ignore)) 
@@ -70,7 +78,7 @@ public class CylinderGrow : EncodingMethod
         pin.InitializeDistance(distance);
         if (anchor != null)
         {
-            pin.InitializeLabel(anchor.name);
+            pin.InitializeLabel(isCorner? "CORNER" : anchor.name);
             OnTriggeredWithAnchor(anchor, contactPoint, other);
         }
 
@@ -88,6 +96,7 @@ public class CylinderGrow : EncodingMethod
     {
         _curGrowSpd = 0f;
         _cylinder.localScale = new Vector3(0, _cylinder.localScale.y, 0);
+        usedColliderSet.Clear();
     }
 
 }
