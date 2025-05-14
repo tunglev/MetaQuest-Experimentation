6c2ee12be79c4378079ce250b5a12059905af628
TungVLe <tunle0801@gmail.com>
2025-05-14 11:56:25 -0400

    minor bug fix and introduct hand cylinder

--- Assets/MainTest/ReachGoalTriggerer.cs
+++ Assets/MainTest/ReachGoalTriggerer.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 
 public class ReachGoalTriggerer : MonoBehaviour
 {
-    
+    private Transform handTransform;
+    private Transform cylinderTransform;
     private void Awake() {
-        var rb = gameObject.AddComponent<Rigidbody>();
+        handTransform = transform;
+        GameObject temp = new ("Hand Cylinder");
+        temp.layer = LayerMask.NameToLayer("Human");
+        cylinderTransform = temp.transform;
+        var rb = temp.AddComponent<Rigidbody>();
         rb.isKinematic = true;
-        var c = gameObject.AddComponent<SphereCollider>();
+        var c = temp.AddComponent<CapsuleCollider>();
         c.radius = 0.08f;
+        c.height = 5;
         c.isTrigger = true;
         tag = "NoSound";
         
     }
+    private void Update()
+    {
+        cylinderTransform.position = handTransform.position;
+    }
     private void OnTriggerEnter(Collider other) {
+        print(other.gameObject.name);
         if (other.gameObject.name.Equals("GOAL_CLD")) {
             OVRInput.SetControllerVibration(0.1f, 0.1f, OVRInput.Controller.RTouch);
             GlobalAudio.Instance.PlaySound("Success");
